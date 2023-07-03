ddd49ad5730a47e497e74ae3397bf3b8accec52a
ahad1033 <a.ahad2232@gmail.com>
2023-07-03 17:45:13 +0600

    banner section modified

--- src/Components/NavBar/Banner/Banner.jsx
+++ src/Components/NavBar/Banner/Banner.jsx
@@ -6,10 +6,10 @@ const Banner = () => {
                 <div className="w-full md:w-1/2 mb-8 md:mb-0">
                     <img src="https://img.freepik.com/free-vector/web-development-programmer-engineering-coding-website-augmented-reality-interface-screens-developer-project-engineer-programming-software-application-design-cartoon-illustration_107791-3863.jpg?w=2000" alt="DEVELOPER" className="mx-auto rounded-lg w-full p-10" />
                 </div>
-                <div className="w-full md:w-1/2 text-center md:text-left pr-60">
-                    <h1 className="text-4xl font-bold mb-4">Ahad Ahamed</h1>
-                    <p className="text-xl mb-4">I am a web developer</p>
-                    <p className="text-lg">
+                <div className="w-full md:w-1/2 text-center lg:text-left md:text-center lg:pr-60">
+                    <h1 className="text-5xl font-bold mb-4">Ahad <span className="font-black" style={{ color: "#f9004d"}}>Ahamed</span></h1>
+                    <p className="text-2xl mb-4 lg:text-left md:text-center font-bold">I am a web developer</p>
+                    <p style={{ color: "#f9004d"}} className="text-lg lg:text-left md:text-center">
                         A Front-End Web Developer is a tech industry professional who builds the front portion of websites
                         that customers, guests, or clients use on a daily basis.
                     </p>
